48e031aca3e25f347f0b10a10a6fca40f3944ba6
zoe <radiopi@raspberrypi.(none)>
2023-01-17 03:29:57 -0800

    updated script

--- lab1/scripts/collect_data.py
+++ lab1/scripts/collect_data.py
@@ -1,12 +1,24 @@
+# collect data from the generator
+
 import ugradio
 import numpy as np
 import matplotlib.pylab as plt
 
+anti_aliasing_filter = True # use the default anti-aliasing filter
+
+save_dir = '/home/radiopi/Desktop/' # directory to save data into
+
+# signal frequency
+sig_freq = 1000 * 1000
+
 # sampling frequency
-freq = 2200*1000
+samp_freq = 1000*1000
 
 # create SDR object
-our_sdr = ugradio.sdr.SDR(sample_rate=freq)
+if anti_aliasing_filter:
+    our_sdr = ugradio.sdr.SDR(sample_rate=freq)
+elif not anti_aliasing_filter: # over-ride the default anti-aliasing filter
+     our_sdr = ugradio.sdr.SDR(sample_rate=freq, fir_coeffs = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2047]))
 our_data = our_sdr.capture_data()
     
-np.save('freq'+str(int(freq/1000)), our_data)
+np.save('nofilter_freq'+str(int(freq/1000)), our_data)
