b9737ba4f016d185f8856cfed4302be5be0e4baf
Quetzal <quetzalcoatl@berkeley.edu>
2023-01-18 15:59:31 -0800

    finished s6 data collection

--- lab2/scripts/collect_data.py
+++ lab2/scripts/collect_data.py
@@ -6,7 +6,7 @@ import matplotlib.pylab as plt
 
 anti_aliasing_filter = True # use the SDR default anti-aliasing filter
 mixing = True # use the SDR mixer (SSB)
-center_freq = (1420.4058 - 1)*1000*1000 # 1420.4058 MHz 
+center_freq = (1420.4058 + 1)*1000*1000 # 1420.4058 MHz 
 
 save_dir = '/home/radiopi/Desktop/' # directory to save data into
 take = ''
@@ -33,7 +33,12 @@ samp_freq = 3200*1000
 #         print('NO mixing with default anti-aliasing filter')
 #         our_sdr = ugradio.sdr.SDR(sample_rate=samp_freq, fir_coeffs = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2047]))
 
+# sname = 's_cal_'
+# sname = 's_cold_'
+# sname = 's_on_'
+sname = 's_off_'
+
+
 our_sdr = ugradio.sdr.SDR(direct=False, center_freq=center_freq, sample_rate=samp_freq, gain=20)
-our_data = our_sdr.capture_data(nsamples=2048, nblocks=512)
-np.save(save_dir+'s_cal_'+str(int(samp_freq/1000))+ take, our_data)
-# np.save(save_dir+'s_cold_'+str(int(samp_freq/1000))+ take, our_data)
+our_data = our_sdr.capture_data(nsamples=2048, nblocks=8192)
+np.save(save_dir+ sname +str(int(samp_freq/1000))+ take, our_data)
