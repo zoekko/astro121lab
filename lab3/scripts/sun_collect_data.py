11665fcd9f19a2a5caacfadcdfe9da3a87575198
Darby McCauley <darbymccauley@berkeley.edu>
2023-03-09 00:56:18 +0000

    updates

--- lab3/scripts/sun_collect_data.py
+++ lab3/scripts/sun_collect_data.py
@@ -12,42 +12,35 @@ import matplotlib.pylab as plt
 import numpy as np
 import snap_spec
 import threading
+import logging
 
 def track():
     i=0
     t0 = astropy.time.Time(time.time(), format='unix')
     t_e = 0
-    print('Tracking start time (JD): ')
-    print(t0)
+    logging.info(f'Tracking start time (JD): {t0}')
     while t_e < total_time:
-        print('')
-        print('Iteration: ')
-        print(i)
+        logging.info('')
+        logging.info(f'Iteration: {i}')
         # get the coordinates of the sun in RA and DEC
         t = astropy.time.Time(time.time(), format='unix')
-        print('Tracking time elapsed (min): ')
         t_e = t - t0
         t_e = t_e * 24 * 60
-        print(t_e)
+        logging.info(f'Tracking time elapsed (min): {t_e}')
         sun = astropy.coordinates.get_sun(t)
         # convert coordinates to alt az (Earth coordinates)
         altaz = astropy.coordinates.AltAz(obstime=t, location=obs) # defines a time and a location on earth
         pointing = sun.transform_to(altaz)
-        print('Sun is currently at: ')
-        print('alt: ')
-        print(pointing.alt.deg)
-        print('az: ')
-        print(pointing.az.deg)
+        logging.info(f'Sun is currently at alt, az: {pointing.alt.deg}, {pointing.az.deg}')
 
         # 3/6/23 EDIT: point interf twice: not necessary 
         try:
             interf.point(pointing.alt.deg, pointing.az.deg) # could set wait=False to do other stuff while the telescope is moving
             interf.point(pointing.alt.deg, pointing.az.deg)
-            print('interf now pointing at: ')
-            print(interf.get_pointing()) # currently where the interfs are pointing
+            logging.info(f'interf now pointing at: {interf.get_pointing()}')
         except(AssertionError):
-            print('Assertion Error: trying again')
-            interf.point(pointing.alt.deg, pointing.az.deg)
+            logging.warning('Assertion Error in pointing: trying again')
+#             interf.point(pointing.alt.deg, pointing.az.deg)
         i += 1
         
 def record():
@@ -55,22 +48,29 @@ def record():
     prev_cnt = None
     t0 = astropy.time.Time(time.time(), format='unix')
     t_e = 0
-    print('Recording start time (JD): ')
-    print(t0)
-    while t_e < total_time:
+    logging.info(f'Recording start time (JD): {t0}')
+#     while t_e < total_time:
+    while True:
         t = astropy.time.Time(time.time(), format='unix')
-        print('Recording time elapsed (min): ')
         t_e = t - t0
         t_e = t_e * 24 * 60
-        print(t_e)
+        logging.info(f'Recording time elapsed (min): {t_e}')
         # collect data
-        data = spec.read_data(prev_cnt=prev_cnt)
-        prev_cnt = data['acc_cnt']
-        all_data = np.append(all_data, data)
-        np.save(save_dir, all_data)
+        try:
+            data = spec.read_data(prev_cnt=prev_cnt)
+            prev_cnt = data['acc_cnt']
+            all_data = np.append(all_data, data)
+            np.save(save_dir, all_data)
+        except(AssertionError):
+            logging.warning('Assertion Error in recording: trying again')
+#             data = spec.read_data(prev_cnt=prev_cnt)
+#             prev_cnt = data['acc_cnt']    
+#             all_data = np.append(all_data, data)
+#             np.save(save_dir, all_data)
     
 if __name__=="__main__":
     
+    logging.basicConfig(filename='/home/pi/Blueberry Pi/astro121lab/lab3/data/sun/full_day_sun_log.log', level=logging.INFO)
     save_dir = '/home/pi/Blueberry Pi/astro121lab/lab3/data/sun/full_day_sun'
     total_time = 8*60 # duration of observation in minutes
 
@@ -84,9 +84,7 @@ if __name__=="__main__":
     thrd.start()
     time.sleep(15) # give interfs time to point to sun
     record()
-    print('')
-    print('Finished collecting data!')
+    logging.info('Finished collecting data!')
     interf.stow() # always finsih every observation with this
     interf.stow()
-    print('interf stowed at: ')
-    print(interf.get_pointing())
+    logging.info(f'interf stowed at: {interf.get_pointing()}')
