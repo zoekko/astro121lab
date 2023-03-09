1d115876a49bbf15a6db03ba70284110ea1b49a6
Zoe Ko <zoeko@ugastro.berkeley.edu>
2023-03-09 14:49:49 -0800

    fixed script

--- lab3/scripts/sun_collect_data.py
+++ lab3/scripts/sun_collect_data.py
@@ -41,16 +41,16 @@ def track():
         except(AssertionError):
             logging.warning('Assertion Error in pointing: trying again')
 #             interf.point(pointing.alt.deg, pointing.az.deg)
-        i += 1
+        i+=1
         
 def record():
-    all_data = np.array([])
+    i=0
     prev_cnt = None
     t0 = astropy.time.Time(time.time(), format='unix')
     t_e = 0
     logging.info(f'Recording start time (JD): {t0}')
-#     while t_e < total_time:
-    while True:
+    while t_e < total_time:
+#     while True:
         t = astropy.time.Time(time.time(), format='unix')
         t_e = t - t0
         t_e = t_e * 24 * 60
@@ -59,19 +59,17 @@ def record():
         try:
             data = spec.read_data(prev_cnt=prev_cnt)
             prev_cnt = data['acc_cnt']
-            all_data = np.append(all_data, data)
-            np.save(save_dir, all_data)
+            np.save(f'{save_dir}{i}', data)
         except(AssertionError):
             logging.warning('Assertion Error in recording: trying again')
-#             data = spec.read_data(prev_cnt=prev_cnt)
-#             prev_cnt = data['acc_cnt']    
-#             all_data = np.append(all_data, data)
-#             np.save(save_dir, all_data)
+            prev_cnt = None
+    i+=1
+           
     
 if __name__=="__main__":
     
     logging.basicConfig(filename='/home/pi/Blueberry Pi/astro121lab/lab3/data/sun/full_day_sun_log.log', level=logging.INFO)
-    save_dir = '/home/pi/Blueberry Pi/astro121lab/lab3/data/sun/full_day_sun'
+    save_dir = '/home/pi/Blueberry Pi/astro121lab/lab3/data/sun/full_day_sun/'
     total_time = 8*60 # duration of observation in minutes
 
     # create interferometer object
@@ -80,9 +78,27 @@ if __name__=="__main__":
     spec.initialize(mode='corr')
     obs = astropy.coordinates.EarthLocation(lon=ugradio.nch.lon, lat=ugradio.nch.lat, height=ugradio.nch.alt)
 
+    # point to sun
+    logging.info(f'First tracking start time (JD): {t0}')
+    t = astropy.time.Time(time.time(), format='unix')
+    sun = astropy.coordinates.get_sun(t)
+    # convert coordinates to alt az (Earth coordinates)
+    altaz = astropy.coordinates.AltAz(obstime=t, location=obs) # defines a time and a location on earth
+    pointing = sun.transform_to(altaz)
+    logging.info(f'Sun is currently at alt, az: {pointing.alt.deg}, {pointing.az.deg}')
+
+    # 3/6/23 EDIT: point interf twice: not necessary 
+    try:
+        interf.point(pointing.alt.deg, pointing.az.deg) # could set wait=False to do other stuff while the telescope is moving
+        interf.point(pointing.alt.deg, pointing.az.deg)
+        logging.info(f'interf now pointing at: {interf.get_pointing()}')
+    except(AssertionError):
+        logging.warning('Assertion Error in FIRST pointing!')
+    
+   
+    # start threading
     thrd = threading.Thread(target=track)
     thrd.start()
-    time.sleep(15) # give interfs time to point to sun
     record()
     logging.info('Finished collecting data!')
     interf.stow() # always finsih every observation with this
