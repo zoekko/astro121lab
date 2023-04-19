680a7dca9a6039a1ef4900ce8cd9e67ed09c17ed
gvo27 <gvo@berkeley.edu>
2023-04-19 13:21:46 -0700

    working on sort

---
+++ lab4/graph.py
@@ -0,0 +1,67 @@
+class Graph(object):
+
+    # Initialize the matrix
+    def __init__(self, size):
+        self.adjMatrix = []
+        for i in range(size):
+            self.adjMatrix.append([0 for i in range(size)])
+        self.size = size
+
+    # Add edges
+    def add_edge(self, v1, v2):
+        if v1 == v2:
+            print("Same vertex %d and %d" % (v1, v2))
+        self.adjMatrix[v1][v2] = 1
+        self.adjMatrix[v2][v1] = 1
+
+    # Remove edges
+    def remove_edge(self, v1, v2):
+        if self.adjMatrix[v1][v2] == 0:
+            print("No edge between %d and %d" % (v1, v2))
+            return
+        self.adjMatrix[v1][v2] = 0
+        self.adjMatrix[v2][v1] = 0
+
+    def __len__(self):
+        return self.size
+
+    # Print the matrix
+    def print_matrix(self):
+        for row in self.adjMatrix:
+            for val in row:
+                print('{:4}'.format(val)),
+            print
+
+from sys import maxsize
+from itertools import permutations
+V = 4
+ 
+# implementation of traveling Salesman Problem
+def travellingSalesmanProblem(graph, s):
+ 
+    # store all vertex apart from source vertex
+    vertex = []
+    for i in range(V): # CHANGE V to graph.shape[something]
+        if i != s:
+            vertex.append(i)
+ 
+    # store minimum weight Hamiltonian Cycle
+    min_path = maxsize
+    next_permutation=permutations(vertex)
+    for i in next_permutation:
+ 
+        # store current Path weight(cost)
+        current_pathweight = 0
+ 
+        # compute current path weight
+        k = s
+        for j in i:
+            current_pathweight += graph[k][j]
+            k = j
+        current_pathweight += graph[k][s]
+ 
+        # update minimum
+        min_path = min(min_path, current_pathweight)
+         
+    return min_path
+
