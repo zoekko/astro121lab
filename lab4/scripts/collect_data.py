2c01cb19e5a4617be3fddf4c060940061b73aed6
Zoe Ko <zoeko@ugastro.berkeley.edu>
2023-04-20 14:57:57 -0700

    debug

--- lab4/scripts/collect_data.py
+++ lab4/scripts/collect_data.py
@@ -1,24 +1,21 @@
 import ugradio
 from ugradio import leusch
-
 import astropy.coordinates
 from astropy.coordinates import AltAz, EarthLocation, SkyCoord
 import astropy.time
 from astropy.time import Time
 import astropy.units as u
-
 from datetime import datetime
 import time
-
 import matplotlib.pylab as plt
 import numpy as np
 import logging
 
-time.sleep(40800)
-
-TEST_ON_MOON = False
+sleep_time = 39600 #s, sleep for this duration of time before beginning to track
+TEST_ON_MOON = False # track moon as a test 
 
 def galactic_to_altaz(coord, time):
+    '''convert galactic coords to altaz coords'''
     l, b = coord
     g_sc = SkyCoord(frame="galactic", l=l*u.deg, b=b*u.deg)
     altaz_sc = g_sc.transform_to(AltAz(obstime=time, location=LOC))
@@ -26,13 +23,19 @@ def galactic_to_altaz(coord, time):
 
 save_dir = '/home/radiolab/Desktop/bpi/astro121lab/lab4/data/'
 logging.basicConfig(filename=f'{save_dir}orion.log', level=logging.INFO)
+
+# go to sleep 
 t = Time(time.time(), format='unix').value
-logging.info(f'First tracking start time (unix time): {t}')
+logging.info(f'Current unix time: {t}')
+logging.info(f'Going to sleep for {sleep_time} seconds.')
+time.sleep(sleep_time)
+t = Time(time.time(), format='unix').value
+logging.info(f'Current unix time: {t}')
 
 # create LeuschTelescope object
 telescope = ugradio.leusch.LeuschTelescope()
 
-# set the LO such that HI is in the center of the bandpass
+# set the LO such that HI shows up at 150 MHz
 LO = 635 #MHz
 synclient = ugradio.agilent.SynthClient()
 synclient.set_frequency(LO, unit='MHz')
@@ -82,13 +85,11 @@ while successful_pointing:
 noisediode.off()
 logging.info('Turned off noise diode')
 
-failed_coords = []
-
+failed_coords = [] # save the coordinates that are out of bounds and go back to them later
 for gal_coord in pointings:
     l, b = gal_coord
     alt, az = galactic_to_altaz(gal_coord, Time(datetime.now()))
-    
-    
+
     if TEST_ON_MOON:
         # get the coordinates of the moon to test
         t = astropy.time.Time(time.time(), format='unix')
@@ -101,7 +102,6 @@ for gal_coord in pointings:
     logging.info(f'alt az set to: {alt}, {az}')
     
     fname = f'{save_dir}{t}_{l}_{b}.fits'
-    N = 10 # try dif values of N and explore how this affects SNR
     spec = ugradio.leusch.Spectrometer()
 
     num_times_failed = 0 # the number of times the telescope failed to point 
@@ -135,7 +135,6 @@ for gal_coord in pointings:
 #     logging.info(f'alt az set to: {alt}, {az}')
     
 #     fname = f'{save_dir}take2_{t}_{l}_{b}.fits'
-#     N = 10 
 #     spec = ugradio.leusch.Spectrometer()
 
 #     successful_pointing=True
@@ -153,7 +152,7 @@ for gal_coord in pointings:
 # george's edit:
 # run on previously failed coordinates
 logging.info(f'Running on previously failed coordinates.')
-compete = False
+complete = False
 while complete==False:
     gal_coord = list(failed_coords).pop(0)
     l, b = gal_coord
@@ -166,23 +165,17 @@ while complete==False:
     logging.info(f'alt az set to: {alt}, {az}')
     
     fname = f'{save_dir}take2_{t}_{l}_{b}.fits'
-    N = 10 
     spec = ugradio.leusch.Spectrometer()
     try:
         telescope.point(alt, az)
         logging.info(f'telescope now pointing at: {telescope.get_pointing()}')
         spec.read_spec(fname, N, (l,b), 'ga')
     except(AssertionError):
-        logging.info(f'Unsuccessful pointing, trying again in a minute')
+        logging.info(f'Unsuccessful pointing, moving onto next failed coordinate in a minute')
         failed_coords += [list(gal_coord)]
-#         time.sleep(60)
+        time.sleep(60)
     if len(failed_coords) == 0:
         complete = True
-     
-            
-            
-            
-            
             
             
 telescope.stow()
